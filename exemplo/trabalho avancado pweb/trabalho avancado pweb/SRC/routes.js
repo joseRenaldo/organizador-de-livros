4fb2c85806f0050d4dcf513d57f0aa7cbbabee83
Aluno IFAL <alunoifal@MACEIO.IFAL>
2026-05-07 14:26:57 -0300

    hii

--- exemplo/trabalho avancado pweb/trabalho avancado pweb/SRC/routes.js
+++ exemplo/trabalho avancado pweb/trabalho avancado pweb/SRC/routes.js
@@ -6,10 +6,12 @@ import cliente from './app/repositorios/cliente.js'
 import Emprestimo from './app/repositorios/emprestimos.js'
 
 const router = router()
+//livro
 router.get('/livros', livrosController.index)
 router.get('/livros/:id', livrosController.show)
 router.post('/livros', livrosController.store)
 router.delete('/livros/:id', livrosController.delete)
 router.put('/livros/:id', livrosController.update)
+//autor
 
 export default router
