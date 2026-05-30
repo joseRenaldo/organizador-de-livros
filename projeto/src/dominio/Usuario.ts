3c24c6a042785165d1ec994d8bb5c292f4e483db
David <dls32@aluno.ifal.edu.br>
2026-05-30 19:23:27 -0300

    Classe usuario add em Usuario.ts

--- projeto/src/dominio/Usuario.ts
+++ projeto/src/dominio/Usuario.ts
@@ -1,1 +1,54 @@
+export abstract class Usuario {
+  #id?: number;
+  #nome: string;
+  #email!: string;
+  #senha: string;
+  #dataNascimento: Date;
+
+  constructor(
+    nome: string,
+    email: string,
+    senha: string,
+    dataNascimento: Date,
+    id?: number,
+  ) {
+    this.#nome = nome;
+    this.setEmail(email);
+    this.#senha = senha;
+    this.#dataNascimento = dataNascimento;
+    this.#id = id;
+  }
+
+  get id() {
+    return this.#id;
+  }
+  get nome() {
+    return this.#nome;
+  }
+  get email() {
+    return this.#email;
+  }
+  get senha() {
+    return this.#senha;
+  }
+  get dataNascimento() {
+    return this.#dataNascimento;
+  }
+
+  setEmail(email: string) {
+    const testeEmail = email.trim();
+
+    const temA = testeEmail.includes("@");
+    const temPonto = testeEmail.includes(".");
+    const semEspaco = testeEmail.includes(" ");
+
+    if (!temA || !temPonto || !semEspaco) {
+      throw new Error("Email inválido");
+    }
+    this.#email = email;
+  }
+
+  abstract getNivelAcasso(): string;
+}
+
 
