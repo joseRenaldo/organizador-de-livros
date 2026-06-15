4362ec7a19120df31274ba1d47d3db7922e1b5d4
joseRenaldo <jrpg1@aluno.ifal.edu.br>
2026-06-14 22:52:29 -0300

    atualizando

--- projeto/src/app/middlewares/authMiddleware.js
+++ projeto/src/app/middlewares/authMiddleware.js
@@ -6,12 +6,12 @@ const JWT_SECRET = "jao_trabalha_pfv";
 export default function authMiddleware(req, res, next) {
     const authHeader = req.headers.authorization;
 
-    // Verificar se o header Authorization existe
+    // Verifica header 
     if (!authHeader) {
         return res.status(401).json({ message: 'Token não fornecido' });
     }
 
-    // O header costuma vir no formato: "Bearer <TOKEN>", vamos dividir a string
+    //split o header
     const parts = authHeader.split(' ');
 
     if (parts.length !== 2 || parts[0] !== 'Bearer') {
@@ -20,19 +20,22 @@ export default function authMiddleware(req, res, next) {
 
     const token = parts[1];
 
-    // Validar o Token
+    // Valida o Token
     jwt.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
             return res.status(401).json({ message: 'Token inválido ou expirado' });
         }
 
-        // Se estiver tudo certo, injetamos os dados decodificados na requisição
+        //injeta os dados decodificados na requisição
         req.usuarioLogado = {
             id: decoded.id,
             email: decoded.email,
             tipo: decoded.tipo
         };
 
-        return next(); // Passa para o próximo controller da rota
+        //              |
+        //              |
+//manda para o proximo  ▽
+                return next(); 
     });
 }
